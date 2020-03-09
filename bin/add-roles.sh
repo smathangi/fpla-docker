dfffa990b09155906ac223b58f0e703776e24f13
Sam Walker <sam.walker@hmcts.net>
2020-03-09 11:02:31 +0000

    feat: added adoption clerk user (FPLA-1226)

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -7,6 +7,7 @@ dir=$(dirname ${0})
 ${dir}/utils/idam-add-role.sh "ccd-import"
 ${dir}/utils/idam-add-role.sh "caseworker"
 ${dir}/utils/idam-add-role.sh "caseworker-publiclaw"
+${dir}/utils/idam-add-role.sh "caseworker-adoption"
 
 # User used during the CCD import and ccd-role creation
 ${dir}/utils/idam-create-caseworker.sh "ccd.docker.default@hmcts.net" "ccd-import"
@@ -16,6 +17,13 @@ for role in "${roles[@]}"
 do
 	${dir}/utils/idam-add-role.sh "caseworker-publiclaw-${role}"
   ${dir}/utils/ccd-add-role.sh "caseworker-publiclaw-${role}"
+done
+
+roles=("clerk")
+for role in "${roles[@]}"
+do
+  ${dir}/utils/idam-add-role.sh "caseworker-adoption-${role}"
+  ${dir}/utils/ccd-add-role.sh "caseworker-adoption-${role}"
 done
 
 ${dir}/utils/ccd-add-role.sh "citizen"
