4bc0c9e88c8fb96c62dd0d76357c93eb67dd6261
Mounika Ammineni <mounikaam@kainos.com@Mounika.local>
2020-03-12 13:29:20 +0000

    Updating redirect url for adoption frontend

--- bin/add-services.sh
+++ bin/add-services.sh
@@ -6,5 +6,5 @@ dir=$(dirname ${0})
 
 ${dir}/utils/idam-create-service.sh "ccd_gateway" "ccd_gateway" "ccd_gateway_secret" "http://localhost:3451/oauth2redirect"
 
-${dir}/utils/idam-create-service.sh "adoption" "adoption" "OOOOOOOOOOOOOOOO" "https://localhost:9000/oauth2/callback"
+${dir}/utils/idam-create-service.sh "adoption" "adoption" "adoption" "https://localhost:3000/landing"
 
